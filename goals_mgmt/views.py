605d5b831ec493769bef793b11ca91ff5170a646
tyler-stone <tjs_rocks@yahoo.com>
2014-01-08 19:35:20 -0500

    Modified home view

--- goals_mgmt/views.py
+++ goals_mgmt/views.py
@@ -7,10 +7,17 @@ from models import Goal
 def home(request):
 	user = request.user.get_full_name
 	user_goals = Goal.objects.filter(owner__username=request.user.username)
+	
 	inbox_goals = user_goals.filter(unread='true')
+	num_inbox_goals = len(inbox_goals)
+	
 	overdue_goals = user_goals.filter(flag__title="Overdue")
+	num_overdue_goals = len(overdue_goals)
+
 	today_goals = user_goals.filter(flag__title="Due Today")
-	return render(request, 'home.html', {'user':user, 'inbox_goals':inbox_goals, 'overdue_goals':overdue_goals, 'today_goals':today_goals})
+	num_today_goals = len(today_goals)
+
+	return render(request, 'home.html', {'user':user, 'inbox_goals':inbox_goals, 'num_inbox_goals':num_inbox_goals, 'overdue_goals':overdue_goals, 'num_overdue_goals':num_overdue_goals, 'today_goals':today_goals, 'num_today_goals':num_today_goals})
 
 def redirect(request):
 	return HttpResponseRedirect('/goals/')
